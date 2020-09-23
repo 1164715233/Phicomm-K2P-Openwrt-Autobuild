2e0f2ef3873b82f6105330b9155ce338016dacaf
1164715233 <71438243+1164715233@users.noreply.github.com>
2020-09-23 04:55:58 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/HiGarfield/lede-17.01.4-Mod.git package/lean/smartdns
-git clone https://github.com/HiGarfield/lede-17.01.4-Mod.git package/lean/luci-app-smartdns
-git clone https://github.com/HiGarfield/lede-17.01.4-Mod.git package/lean/luci-app-eqos
+git clone https://github.com/liuran001/openwrt-packages.git package/lean/smartdns
+git clone https://github.com/liuran001/openwrt-packages.git package/lean/luci-app-smartdns
+git clone https://github.com/liuran001/openwrt-packages.git package/lean/luci-app-eqos
