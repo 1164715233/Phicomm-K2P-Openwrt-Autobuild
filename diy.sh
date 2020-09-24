91c32575bd50e08553f847ebf3b912b34ac7f346
1164715233 <71438243+1164715233@users.noreply.github.com>
2020-09-24 07:16:39 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/liuran001/openwrt-packages.git
-
+git clone https://github.com/liuran001/openwrt-packages.git package/smartdns
+git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-smartdns
+git clone https://github.com/liuran001/openwrt-packages.git package/luci-app-eqos
